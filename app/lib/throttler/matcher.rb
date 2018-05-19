aa4f8639f941777089adacef802b884e38dc36bf
v-sh <vladimirshushlin@mail.ru>
2018-05-19 20:47:19 +0300

    msg fix

--- app/lib/throttler/matcher.rb
+++ app/lib/throttler/matcher.rb
@@ -22,9 +22,9 @@ class Throttler
         end
       rescue IPAddr::InvalidAddressError
         msg =  "incorrect matcher #{ip},\n"\
-               'possible options:\n'\
-               '10.0.0.0/24\n'\
-               '127.0.0.1\n'\
+               "possible options:\n"\
+               "10.0.0.0/24\n"\
+               "127.0.0.1\n"\
                "'my-custom-matcher' {|req| req.ip == '127.0.0.1'}"
         raise ArgumentError, msg
       end
